da6d932890c72430ac02595fdc62ca0834295a51
Danieleclima <daniel.echuaca.lima@gmail.com>
2020-04-30 20:28:43 +0100

    changed background color on nightclub page

--- client/src/components/NightClub.js
+++ client/src/components/NightClub.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Maya from '../assets/Mayaclubbio.jpg'
-import bsinit from '../scripts/bs-init'
-import jquery from '../scripts/jquery.min.js'
+import $ from "jquery"
+// import bsinit from '../scripts/bs-init.js'
+// import jquery from '../scripts/jquery.min.js'
+// import bootstrap from '../scripts/bootstrap.min.js'
 import '../NightClubPage.css'
 
 export default class NightClub extends Component {
@@ -31,24 +33,21 @@ export default class NightClub extends Component {
                             <div class="container nighclub-page-background-color container-fluid">
                                 <div class="row nightclub-page-section">
                                     <div class="col-sm-12 col-lg-8 col-xl-8 d-flex justify-content-center align-content-center justify-content-md-center justify-content-lg-end justify-content-xl-end ratings-and-reviews">
-                                        <aside class="border rounded shadow-sm ratings-and-reviews-component" data-bs-hover-animate="pulse"></aside>
+                                        <aside class="border rounded shadow-sm ratings-and-reviews-component"></aside>
                                     </div>
                                     <div class="col-12 col-sm-12 col-lg-4 col-xl-4 d-flex justify-content-center justify-content-lg-start justify-content-xl-start location-section">
-                                        <aside class="border rounded shadow-sm location-component" data-bs-hover-animate="pulse"></aside>
+                                        <aside class="border rounded shadow-sm location-component"></aside>
                                     </div>
                                     <div class="col-sm-12 col-lg-8 col-xl-8 d-flex flex-column justify-content-center align-items-center align-items-sm-center justify-content-md-center justify-content-lg-end align-items-lg-end justify-content-xl-end align-items-xl-end details-and-amenities-section">
-                                        <aside class="border rounded shadow-sm mb-auto details-component" data-bs-hover-animate="pulse"></aside>
-                                        <aside class="border rounded shadow-sm d-flex reviews-component" data-bs-hover-animate="pulse"></aside>
+                                        <aside class="border rounded shadow-sm mb-auto details-component"></aside>
+                                        <aside class="border rounded shadow-sm d-flex reviews-component"></aside>
                                     </div>
                                     <div class="col-sm-12 col-lg-4 col-xl-4 d-flex justify-content-center order-sm-1 justify-content-lg-start ml-lg-auto justify-content-xl-start ml-xl-auto nearby-section">
-                                        <aside class="border rounded shadow-sm map-component" data-bs-hover-animate="pulse"></aside>
+                                        <aside class="border rounded shadow-sm map-component"></aside>
                                     </div>
                                 </div>
                             </div>                           
-                            <script src={jquery}></script>
-                            {/* <script src="assets/bootstrap/js/bootstrap.min.js"></script> */}
-                            <script src={bsinit}></script>
-                
+                            <script src='../scripts/bs-init.js'></script>        
             </React.Fragment>
         )
     }
